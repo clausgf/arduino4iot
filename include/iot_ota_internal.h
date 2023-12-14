585edb98f637b05488e695263d2177ccc9f0f4cf
Philipp Köhler <phi.koehler@ostfalia.de>
2023-12-14 12:12:05 +0100

    move esp callbacks

--- include/iot_ota_internal.h
+++ include/iot_ota_internal.h
@@ -35,10 +35,4 @@ private:
     const char * _server_cert_pem;
     bool _skip_server_common_name_check;
     int _timeout_ms;
-
-    static void _setFirmwareHttpEtag(const char* etag) noexcept;
-    static void _setFirmwareHttpDate(const char* date) noexcept;
-
-    static esp_err_t _http_client_init_cb(esp_http_client_handle_t http_client) noexcept;
-    static esp_err_t _http_event_handler(esp_http_client_event_t *evt) noexcept;
 };
